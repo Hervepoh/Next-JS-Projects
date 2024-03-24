2032b354eec5212f83add0361dcc76e0c74bbb3a
Jbtzcodes <laminutedecode@gmail.com>
2024-03-24 19:47:41 +0100

    mise a jour

--- 01-Pinterest-clone/next.config.mjs
+++ 01-Pinterest-clone/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
-//     Ces lignes de configuration sont utilisées dans Next.js pour spécifier les domaines à partir desquels les images peuvent être chargées.
+
 
 // Dans ce cas précis, la configuration indique que les images peuvent être chargées à partir du domaine "lh3.googleusercontent.com". Cela signifie que si vous avez des images hébergées sur ce domaine, Next.js les autorisera à être affichées dans votre application. Cela peut être utile si vous avez des images stockées sur Google Cloud Storage et que vous souhaitez les utiliser dans votre application Next.js.
 
